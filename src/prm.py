4ad2868a1bad91676b7465b6670bb324f2ed5afa
Maciej <maciej.skwara2@gmail.com>
2020-09-11 19:06:53 +0200

    check_if_valid method added, find_closest improve

--- src/prm.py
+++ src/prm.py
@@ -26,16 +26,72 @@ class PRM(GridMap):
             if (self.map[Yc][Xc] != 100):
                 self.points_list.append((x[0], y[0]))
 
-
     def find_closest(self,r):
-        for point1 in self.points_list:
-            for point2 in self.points_list:
+        distance_list = []
+        for index1, point1 in enumerate(self.points_list):
+            for index2, point2 in enumerate(self.points_list):
                 distance = np.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-                if (distance <= r) and (point1 != point2):
-                    print("point1", point1, "point2", point2, "distance", distance)
-                    self.prm_points_to_connection.append((point1,point2))
 
-        # print(self.parent)
+                # checking if the point is in range
+                # does the point not equal itself
+                # there is no obstacle between the points
+                # whether the distance between points was not repeated
+                # (prevents the same points from connecting a second time
+                if (distance <= r) and \
+                        (point1 != point2) and \
+                        prm.check_if_valid(point1, point2) and \
+                        (distance not in distance_list):
+                    # print("point1", point1, "point2", point2, "distance", distance)
+                    self.prm_points_to_connection.append((point1,point2))
+                distance_list.append(distance)
+
+    def check_if_valid(self, point1, point2):
+        ilosc_probek = 100
+        dlugosc = np.sqrt((point1[0] - point2[0]) * (point1[0] - point2[0]) + (point1[1] - point2[1]) * (point1[1] - point2[1]))
+
+        lista = []
+        Yc = point1[1]
+        Xc = point1[0]
+        step_x = abs(point1[0] - point2[0]) / ilosc_probek
+        step_y = abs(point1[1] - point2[1]) / ilosc_probek
+
+        if (point2[1] > Yc):
+            Yc = ((step_y * abs(point2[1] - Yc)) / (dlugosc)) + Yc
+
+        if (point2[1] < Yc):
+            Yc = (-1 * ((step_y * abs(point2[1] - Yc)) / (dlugosc))) + Yc
+
+        if (point2[0] > Xc):
+            Xc = ((step_x * abs(point2[0] - Xc)) / (dlugosc)) + Xc
+
+        if (point2[0] < Xc):
+            Xc = (-1 * ((step_x * abs(point2[0] - Xc)) / (dlugosc))) + Xc
+        lista.append([Xc, Yc])
+
+        for i in range(ilosc_probek - 1):
+
+            if (point2[1] > Yc):
+                Yc = Yc + step_y
+            if (point2[1] < Yc):
+                Yc = Yc - step_y
+            if (point2[0] > Xc):
+                Xc = Xc + step_x
+            if (point2[0] < Xc):
+                Xc = Xc - step_x
+
+            lista.append([Xc, Yc])
+
+        for i in range(ilosc_probek):
+            punkt = lista[i]
+            Xc = int(punkt[0] * 10)
+            Yc = int(punkt[1] * 10)
+
+            if (self.map[Yc][Xc] == 100):
+                in_free_space = False
+                return in_free_space
+
+        in_free_space = True
+        return in_free_space
 
 
 
@@ -46,17 +102,17 @@ class PRM(GridMap):
         path = []
 
         # number of samples
-        n = 30
+        n = 200
         prm.sampling(n)
-        # print(self.start)
 
         # show points on map
         self.publish_points()
 
 
-        r = 1.0
+        r = 0.50
         prm.find_closest(r)
         self.prm_publish_connections()
+
         while not rp.is_shutdown():
             # Search for neighbors only in a limited neighborhood, radius r
 
