c450f0c06c6ef7894e801ddf66fb41b3a645d286
Maciej <maciej.skwara2@gmail.com>
2020-09-16 10:56:28 +0200

    update checking_connections method

--- src/rrt_star.py
+++ src/rrt_star.py
@@ -13,16 +13,14 @@ class RRT_star(GridMap):
         super(RRT_star, self).__init__()
         self.parent_distance = {}
         self.step = 0.2
+        self.neighborhood_step = 0.25
+
     def random_point(self):
-        """
-        Draws random point in 2D
-        :return: point in 2D
-        """
         x = self.width * np.random.random(1)
         y = self.height * np.random.random(1)
-        x = round(x, 4)
-        y = round(y, 4)
-        point = (x, y)
+        # x = round(x, 4)
+        # y = round(y, 4)
+        point = (x[0], y[0])
         return point
 
     def find_closest(self, pos):
@@ -40,8 +38,8 @@ class RRT_star(GridMap):
                 x = wartosc[0]
                 y = wartosc[1]
 
-        x = round(x, 4)
-        y = round(y, 4)
+        # x = round(x, 4)
+        # y = round(y, 4)
         closest = (x, y)
         return closest
 
@@ -93,27 +91,6 @@ class RRT_star(GridMap):
         in_free_space = True
         return in_free_space
 
-    def checking_connections(self, random_point):
-        # print(self.parent)
-        distance = 0
-        min = 1000  # duza wartosc
-        for i, value in enumerate(self.parent):
-            if rrt_star.check_if_valid(random_point, value):
-                distance = np.sqrt((random_point[0] - value[0]) ** 2 + (random_point[1] - value[1]) ** 2) + self.parent_distance[value]
-                if (min > distance):
-                    min = distance
-                    x = value[0]
-                    y = value[1]
-            else:
-                pass
-        if distance != 0:
-            # print(random_point, (x, y), distance)
-            self.parent_distance[random_point] = distance
-            self.parent[random_point] = (x, y)
-            distance = 0
-
-        # print(self.parent_distance)
-
     def new_pt(self, pt, closest):
         b = pt
         a = closest
@@ -129,11 +106,35 @@ class RRT_star(GridMap):
         if (b[0] < a[0]):
             Xc = (-1 * ((self.step * abs(b[0] - a[0])) / (dlugosc))) + a[0]
 
-        Xc = round(Xc, 4)
-        Yc = round(Yc, 4)
+        # Xc = round(Xc, 4)
+        # Yc = round(Yc, 4)
         pt = (Xc, Yc)
         return pt
 
+
+    def checking_connections(self, random_point_in_self_step):
+        cost_list = []
+        for i, value in enumerate(self.parent):
+            # calculate distance/cost from random point to every point in parent dict
+            distance = np.sqrt((random_point_in_self_step[0] - value[0]) ** 2 + (random_point_in_self_step[1] - value[1]) ** 2)
+            if distance < self.neighborhood_step:
+                # calculate cost
+                cost = distance + self.parent_distance[value]
+                # add cost to cost list
+                cost_list.append((cost, random_point_in_self_step, value))
+
+        # find lowes cost
+        cost_list = sorted(cost_list)
+        lowest_cost = cost_list[0][0]
+        # add lowest cost do cost dict
+        self.parent_distance[cost_list[0][1]] = lowest_cost
+        # add point with lowest cost to parent dict
+        self.parent[cost_list[0][1]] = cost_list[0][2]
+
+
+
+    def chose_parent(self):
+        pass
 
     def search(self):
         print("RRT*")
@@ -149,10 +150,13 @@ class RRT_star(GridMap):
 
             random_point = rrt_star.random_point()
             closest_point = rrt_star.find_closest(random_point)
+            random_point_in_self_step = rrt_star.new_pt(random_point, closest_point)
 
-            rrt_star.checking_connections(random_point)
+            if rrt_star.check_if_valid(random_point_in_self_step, closest_point):
+                rrt_star.checking_connections(random_point_in_self_step)
+
             self.publish_search()
-            rp.sleep(0.10)
+            rp.sleep(0.50)
 
 
 
